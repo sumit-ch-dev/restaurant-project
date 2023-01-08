4cf22a8e5e99ae8960f7aa83f3c5b6ebfa92ffbf
symit1729 <symit1729@outlook.com>
2023-01-08 15:16:26 +0600

    modified:   src/components/body/Menu.js 	modified:   src/components/body/MenuItem.js

--- src/components/body/MenuItem.js
+++ src/components/body/MenuItem.js
@@ -4,15 +4,19 @@ import { Card, CardImg, CardImgOverlay, CardBody, CardTitle } from 'reactstrap';
 const MenuItem = props => {
     return (
         <div>
-            <Card style={{margin: "10px"}}>
+            <Card style={{ margin: "10px" }}>
                 <CardBody>
-                    <CardImg 
-                    width="100%" 
-                    alt={props.dish.name} 
-                    src={props.dish.image}
-                    style={{opacity: "0.5"}}/>
+                    <CardImg
+                        width="100%"
+                        alt={props.dish.name}
+                        src={props.dish.image}
+                        style={{ opacity: "0.5" }} />
                     <CardImgOverlay>
-                        <CardTitle>{props.dish.name}</CardTitle>
+                        <CardTitle
+                            style={{ cursor: "pointer" }}
+                            onClick={props.DishSelect}>
+                            {props.dish.name}
+                        </CardTitle>
                     </CardImgOverlay>
                 </CardBody>
             </Card>
