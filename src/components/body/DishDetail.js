3608d5e329048106da2c2d26bd8c28581fdafb92
symit1729 <symit1729@outlook.com>
2023-02-01 07:33:34 +0600

    modified:   src/components/body/DishDetail.js 	new file:   src/components/body/LoadComments.js

--- src/components/body/DishDetail.js
+++ src/components/body/DishDetail.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Card, CardImg, CardBody, CardTitle, CardText } from 'reactstrap';
+import LoadComments from "./LoadComments";
 
 const DishDetail = props => {
     return (
@@ -10,8 +11,10 @@ const DishDetail = props => {
                     <CardTitle>{props.dish.name}</CardTitle>
                     <CardText>
                         <p>{props.dish.description}</p>
-                        <p>{props.dish.price}</p>
+                        <p>Price: {props.dish.price} /-</p>
                     </CardText>
+                    <hr/>
+                    <LoadComments comments={props.dish.comments}/>
                 </CardBody>
             </Card>
         </div>
