5879a402398de36cfe34e794ba2be69b20ec6b90
symit1729 <symit1729@outlook.com>
2023-02-28 15:15:37 +0600

    modified:   package-lock.json 	modified:   package.json 	modified:   src/App.js 	new file:   src/components/body/About.js 	modified:   src/components/body/Body.js 	new file:   src/components/body/Contact.js 	new file:   src/components/body/Home.js 	modified:   src/components/body/Menu.js 	modified:   src/components/header/Navigation.js

--- src/components/body/Body.js
+++ src/components/body/Body.js
@@ -1,5 +1,9 @@
 import React from "react";
 import Menu from "./Menu";
+import Home from "./Home";
+import Contact from "./Contact";
+import About from "./About";
+
 
 const Body = () => {
     return (
--- src/components/header/Navigation.js
+++ src/components/header/Navigation.js
@@ -1,16 +1,31 @@
-import React from "react";
-import { Navbar, NavbarBrand } from 'reactstrap';
+import React, { Component } from "react";
+import { Navbar, NavbarBrand, Nav, NavItem, NavbarToggler, Collapses } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
-const Navigation = () => {
-    return (
-        <div>
-            <Navbar dark color="dark">
-                <div className="container">
-                    <NavbarBrand href="/">Saladia Restaurant</NavbarBrand>
-                </div>
-            </Navbar>
-        </div>
-    );
+class Navigation extends Component {
+    render() {
+        return (
+            <div>
+                <Navbar dark color="dark">
+                    <div className="container">
+                        <NavbarBrand href="/">Saladia Restaurant</NavbarBrand>
+                        <Nav className="mr-auto" navbar>
+                            <NavItem>
+                                <Link to="/" className="nav-link active">Home</Link>
+                            </NavItem>
+                            <NavItem>
+                                <Link to="/menu" className="nav-link">Menu</Link>
+                            </NavItem>
+                            <NavItem>
+                                <Link to="/about" className="nav-link">About</Link>
+                            </NavItem>
+                        </Nav>
+                    </div>
+                </Navbar>
+            </div>
+        )
+    }
 }
+
 
 export default Navigation;
