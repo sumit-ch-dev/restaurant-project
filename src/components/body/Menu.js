c7b77dcbace15f131d3b7d5067b92e06d5eafb8f
symit1729 <symit1729@outlook.com>
2023-03-10 21:58:13 +0600

    modified:   package-lock.json 	modified:   package.json 	modified:   public/index.html 	modified:   src/components/body/Menu.js

--- src/components/body/Menu.js
+++ src/components/body/Menu.js
@@ -2,17 +2,27 @@ import React, { Component } from 'react';
 import DISHES from '../../data/dishes';
 import MenuItem from './MenuItem';
 import DishDetail from './DishDetail';
-import { CardColumns, Modal, ModalBody, ModalFooter, Button } from 'reactstrap';
+import { Card, CardColumns, Modal, ModalBody, ModalFooter, Button } from 'reactstrap';
 
 class Menu extends Component {
     state = {
         dishes: DISHES,
-        selectedDish: null
+        selectedDish: null,
+        modalOpen: false
     }
 
     onDishSelect = dish => {
         //console.log(dish);
-        this.setState({ selectedDish: dish })
+        this.setState({
+            selectedDish: dish,
+            modalOpen: !this.state.modalOpen
+        })
+    }
+
+    toggleModal = () => {
+        this.setState({
+            modalOpen: !this.state.modalOpen
+        })
     }
 
     render() {
@@ -27,16 +37,25 @@ class Menu extends Component {
         })
 
         let dishDetail = null;
-        if(this.state.selectedDish != null) {
+        if (this.state.selectedDish != null) {
             dishDetail = <DishDetail dish={this.state.selectedDish} />
         }
         return (
             <div className='container'>
                 <div className='row'>
-                    <CardColumns>
-                        {menu}
-                    </CardColumns>
-                    
+                    <div className='col-md-auto'>
+                        <CardColumns>
+                            {menu}
+                        </CardColumns>
+                    </div>
+                    <Modal isOpen={this.state.modalOpen} onClick={this.toggleModal}>
+                        <ModalBody>
+                            {dishDetail}
+                        </ModalBody>
+                        <ModalFooter>
+                            <Button color='secondary' onClick={this.toggleModal}>Close</Button>
+                        </ModalFooter>
+                    </Modal>
                 </div>
             </div>
         );
